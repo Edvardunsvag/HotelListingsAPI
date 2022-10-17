040689d7de60f3c2a19bcbf14fe4606bb91ed75f
Edvard Unsvåg <edvardunsvag@Edvards-MacBook-Pro.local>
2022-10-17 16:03:18 +0200

    created migration to azure

---
+++ HotelListingAPI/data/Country.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace HotelListingAPI.data
+{
+    public class Country
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string ShortName { get; set; }
+
+        public virtual IList<Hotel> Hotels { get; set; }
+
+
+
+
+    }
+}
